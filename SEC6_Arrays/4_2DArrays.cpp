af7345d0ba80e95cc54ac63bd54c01a89ec3b130
vamsi-pallapolu <vamsikrishnanaidu16@gmail.com>
2026-07-30 21:39:05 +0530

    Add pointer-based 2D array examples

--- SEC6_Arrays/4_2DArrays.cpp
+++ SEC6_Arrays/4_2DArrays.cpp
@@ -4,10 +4,20 @@
 int main(){
 
     int A[2][3] = {{1, 2, 3},{4, 5, 6}};
+
+    int *B[2]; // Array of 2 integer pointers
+    B[0] = new int[3];
+    B[1] = new int[3];
+    
+    // double pointer
+    int **C = new int*[2];
+    C[0] = new int[3];
+    C[1] = new int[3];
+
     std::cout << std::endl;
     for(int i=0;i<2;i++){
         for(int j=0;j<3;j++){
-            printf("%d", A[i][j]);
+            printf("%d", C[i][j]);
         };
         std::cout << std::endl;
     }
